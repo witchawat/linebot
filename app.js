68b18721e6c71a0c5c7d03e1243474243fe7c9b6
pantoofski <chaichana.n@gmail.com>
2017-08-02 16:26:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+const redis = require("redis");
+var redisClient = redis.createClient(process.env.REDISCLOUD_URL, {
+  no_ready_check: true
+});
 const line = require('@line/bot-sdk');
 var CronJob = require('cron').CronJob;
 var cloudinary = require('cloudinary'); //gif to mp4
@@ -43,6 +51,23 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     );
 });
 
+app.post('/fb', bodyParser.urlencoded({
+  extended: true
+}), (req, res) => {
+  if (req.body.fbId && req.body.fbToken && req.body.fbId == process.env.EXPECTED_FB_ID) {
+    request.get("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=" + process.env.FB_APP_ID + "&client_secret=" + process.env.FB_APP_SECRET + "&fb_exchange_token=" + req.body.fbToken, function (err, response, body) {
+      var ret = JSON.parse(body);
+      if (!ret.error) {
+        console.log('-- saving fb long token --');
+        redisClient.set('fbToken', ret.access_token, 'EX', ret.expires_in - 60 * 60);
+        fbCron.start();
+      }
+      res.send('ok');
+    });
+  } else {
+    res.send('ok');
+  }
+});
 const client = new line.Client(config);
 var url_radarvid = "";
 var url_radar240 = "";
@@ -270,3 +295,78 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
   var lottoRes = {"prize":{"prize_1":"756519","prize_2":["041938","173091","042845","062174","219884"],"prize_3":["267271","275503","395048","632363","007796","946999","484725","878635","569285","083769"],"prize_4":["563783","062733","039972","491283","420988","134990","520884","892796","325835","251024","137645","163593","459671","516477","468365","069686","093823","020121","344126","359454","131825","752516","662429","551240","682387","057379","661963","506953","138420","902349","382981","837031","673149","935699","854714","681124","041809","799366","618387","571436","179457","959667","713643","965006","946172","331560","930813","167502","988721","929369"],"prize_5":["007542","485463","638123","601114","679839","292585","617811","668434","900336","863341","371905","166751","642022","753076","833905","438244","833531","328828","149705","791628","054209","785644","743496","370208","698212","275428","204511","206021","182409","065139","445313","557057","550918","010147","355296","351045","404835","807351","210536","802362","668513","508349","287935","610782","833125","443565","829636","089115","898319","094111","627599","185188","114932","913713","750766","878853","162885","591519","507291","420506","685955","169235","059828","572490","082779","712843","317127","220342","765988","609875","712284","509567","883974","061881","747385","357270","430714","840855","109888","629873","421124","924989","692935","512441","903764","163973","736364","536854","975039","082194","730527","280009","795262","615146","189725","743123","850989","576528","726550","323603"],"prize1_close":["756518","756520"],"prize_last2":"36","prize_last3":["787","989"],"prize_first3":["061","386"]},"wording":{"prize_1":{"message":"\u0e17\u0e35\u0e48 1","prize":"3,000,000"},"prize_2":{"message":"\u0e17\u0e35\u0e48 2","prize":"100,000"},"prize1_close":{"message":"\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e48 1","prize":"50,000"},"prize_3":{"message":"\u0e17\u0e35\u0e48 3","prize":"40,000"},"prize_4":{"message":"\u0e17\u0e35\u0e48 4","prize":"20,000"},"prize_5":{"message":"\u0e17\u0e35\u0e48 5","prize":"10,000"},"prize_first3":{"message":"\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e32 3 \u0e15\u0e31\u0e27","prize":"2,000"},"prize_last3":{"message":"\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e22 3 \u0e15\u0e31\u0e27","prize":"2,000"},"prize_last2":{"message":"\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e22 2 \u0e15\u0e31\u0e27","prize":"1,000"}}};
+
+
+function FBaggregator() {
+  console.log('-- fbCron --');
+  console.log(new Date());
+  redisClient.get('fbToken', function (err, fbToken) {
+    if (err) {
+      console.log('redis error');
+      return false;
+    }
+    if (typeof fbToken == 'undefined' || !fbToken) {
+      console.log('fbToken is empty');
+      return false;
+    }
+    console.log(fbToken);
+    request.get("https://graph.facebook.com/v2.10/219520224912468/feed?fields=message,permalink_url,updated_time,created_time,full_picture&limit=30&access_token=" + fbToken, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      var ret = JSON.parse(body);
+      if (ret.data) ret.data.forEach(processSalePost);
+      if (ret.error) {
+        client.pushMessage(process.env.LINE_NOTIFY_ID, {
+          type: 'text',
+          'text': 'graph api ร่วง'
+        });
+        fbCron.stop();
+      }
+    });
+  });
+}
+var fbCron = new CronJob({
+  cronTime: '0,30 * * * * *',
+  onTick: FBaggregator,
+  start: true,
+  timeZone: 'Asia/Bangkok',
+  runOnInit: true
+});
+
+function processSalePost(o) {
+  var uId = 'sale.' + o.id + '.' + new Date(o.created_time).getTime();
+  var message = {
+    "type": "template",
+    "altText": "",
+    "template": {
+      "type": "buttons",
+      "text": "",
+      "actions": [{
+        "type": "uri",
+        "label": "เปิดดู",
+        "uri": ""
+      }]
+    }
+  };
+  var msg = o.message.split('\n');
+  if (!msg[1]) return;
+  if (msg[0].indexOf('ตั้งรับ') >= 0 || msg[0].indexOf('ขายแล้ว') >= 0 || msg[0].toLowerCase().indexOf('sold') >= 0) return;
+  if (msg[1].indexOf('฿') < 0 && msg[1].indexOf('FREE') < 0) return;
+  message.altText = message.template.text = msg[0] + ' - ' + msg[1].split(' ')[0];
+  if (o.permalink_url) message.template.actions[0].uri = o.permalink_url;
+  if (o.full_picture) message.template.thumbnailImageUrl = o.full_picture
+  redisClient.get(uId, function (err, uIdVal) {
+    if (err) {
+      console.log('redis error');
+      return false;
+    }
+    if (typeof uIdVal == 'undefined' || !uIdVal) {
+      console.log(uId);
+      console.log(message);
+      client.pushMessage(process.env.LINE_SALE_POST_NOTI_TARGET, message);
+      redisClient.set(uId, 1, 'EX', 7 * 24 * 60 * 60);
+    }
+  });
+}
