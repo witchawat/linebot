31aa28b39d8269b327b707b91c2879d43775c285
witchawat <chiez39@gmail.com>
2017-08-29 12:59:19 +0700

    utmbRunner func with callback

--- utmbrunner.js
+++ utmbrunner.js
@@ -1,27 +1,43 @@
 var request = require('request');
 var _       = require('underscore');
 
-function utmbrunner(bib){
-  var result = request.get('https://service.chillingtrail.run/result/2017/utmb/' + bib, function(error, response, body){
-  var runinfo = JSON.parse(body);
-  var jsonsize = _.size(runinfo.points);
-
-  var rname = runinfo.identity.prenom;
-  var rsurname = runinfo.identity.nom;
-
+function utmbRunner(bib, callback){
+  var runnerinfo ='waiting';
+  var rname = '';
+  var rsurname = '';
   var cpname = [];
   var cptod = [];
   var cptime = [];
-  for(var i = 0 ; i < jsonsize ; i++){
-    if(runinfo.points[i].tps !== undefined){
-      cpname.push(runinfo.points[i].n);
-      cptod.push(runinfo.points[i].ha);
-      cptime.push(runinfo.points[i].tps);
-    };
-  };
-  var info = "BIB: " + bib + "\nName: " + rname + " " + rsurname + "\nLast Checkpoint\n" + _.last(cpname) +"\nTime: " + _.last(cptime) ;
-  return info;
-});
-}; // end of func utmbrunner
 
-module.exports = utmbrunner; 
+  request.get('https://service.chillingtrail.run/result/2017/utmb/' + bib, function(error, response, body){
+    if(response.statusCode == 200) {
+    var runinfo = JSON.parse(body);
+    var rname = runinfo.identity.prenom;
+    var rsurname = runinfo.identity.nom;
+    var jsonsize = _.size(runinfo.points);
+
+    for(var i = 0 ; i < jsonsize ; i++){
+      if(runinfo.points[i].tps !== undefined){
+        cpname.push(runinfo.points[i].n);
+        cptod.push(runinfo.points[i].ha);
+        cptime.push(runinfo.points[i].tps);
+      }
+    }
+    runnerinfo = "BIB: " + bib + "\nName: " + rname + " " + rsurname + "\nLast Checkpoint\n" + _.last(cpname) +"\nTime: " + _.last(cptime) ;
+    callback(null, runnerinfo);
+  } else { // resp != 200
+    console.log('error');
+  }
+
+});
+} // end of func utmbrunner
+
+module.exports = utmbRunner;
+
+// utmbRunner(20100,function(err, ri){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(ri);
+//   }
+// });
