f8fdf557746ab607ddbfd04cd4dad1f289aa0db6
witchawat <chiez39@gmail.com>
2017-08-29 13:23:49 +0700

    added km info

--- utmbrunner.js
+++ utmbrunner.js
@@ -8,22 +8,26 @@ function utmbRunner(bib, callback){
   var cpname = [];
   var cptod = [];
   var cptime = [];
+  var km = [];
 
   request.get('https://service.chillingtrail.run/result/2017/utmb/' + bib, function(error, response, body){
     if(response.statusCode == 200) {
     var runinfo = JSON.parse(body);
-    var rname = runinfo.identity.prenom || runinfo.identity.pnom;
+    var rname = runinfo.identity.prenom || runinfo.identity.pnom || "noname";
     var rsurname = runinfo.identity.nom;
     var jsonsize = _.size(runinfo.points);
 
+    //get current runner's info
     for(var i = 0 ; i < jsonsize ; i++){
       if(runinfo.points[i].tps !== undefined){
         cpname.push(runinfo.points[i].n);
         cptod.push(runinfo.points[i].ha);
         cptime.push(runinfo.points[i].tps);
+        km.push(runinfo.points[i].km);
       }
     }
-    runnerinfo = "BIB: " + bib + "\nName: " + rname + " " + rsurname + "\nLast Checkpoint\n" + _.last(cpname) +"\nTime: " + _.last(cptime) ;
+    runnerinfo = "BIB: " + bib + "\nName: " + rname + " " + rsurname + "\n\nLast Checkpoint\n" + _.last(cpname) +"\nTime: " + _.last(cptime)
+      +"\n\nAt " +_.last(km) + "/" +runinfo.points[jsonsize-1].km +" km";
     callback(null, runnerinfo);
   } else { // resp != 200
     console.log('error');
