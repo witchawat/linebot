573b1524cb46bfa2dc54a7a57961a445b9f8cd63
witchawat <chiez39@gmail.com>
2017-08-29 11:07:58 +0700

    utmbrunner

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var htmlparser = require("htmlparser2");//ไว้ parse ผลหวย
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
-import utmbrunner from 'utmbrunner';
+var utmbrunner from './utmbrunner';
 //================================
 //        KEYS
 //================================
