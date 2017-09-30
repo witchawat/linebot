96aaf9b28546c8bc034fb000ddf4530dfbb74c6d
Witchawat Burapavong <chiez39@gmail.com>
2017-09-30 14:35:16 +0700

    text cd

--- app.js
+++ app.js
@@ -134,15 +134,15 @@ function handleEvent(event) {
     }
 
     return client.replyMessage(event.replyToken, {
-      "type": "text",
-      "text":
+      type : "text",
+      text :
         "Race Registration Countdown\n
-        UTMF " + moment().to(registerTime.utmf) "\n
+        UTMF " + moment().to(registerTime.utmf) + "\n
         UTMF 165K/7500m/46hr\n
         STY 72K/3700m/20hr\n
         http://www.ultratrailmtfuji.com/en/about/regarding/\n\n
 
-        UTMB " + moment().to(registerTime.utmb) "\n
+        UTMB " + moment().to(registerTime.utmb) + "\n
         UTMB 171K/10300m\n
         CCC 101K/6100m\n
         TDS 119K/7200m\n
