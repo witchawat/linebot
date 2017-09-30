af0ccd59f7a141750394e2b6970b4f4e012db7c5
Witchawat Burapavong <chiez39@gmail.com>
2017-09-30 17:02:00 +0700

    f

--- raceregis.js
+++ raceregis.js
@@ -18,28 +18,27 @@ var Race = function(txt, uid, displayname){
   }
   } else {
     if (txt !== "22" || txt !== "44" || txt !== "66" || txt !== "100"){
-      return  {
-        type: 'text',
-        text: 'กรุณาใส่เฉพาะตัวเลข\n22 44 66 100'
-    }
+      Pyt.update({line_userId: uid}, {$set: {line_userId: uid, line_displayName: displayname, distance: txt}}, {upsert: true}, function(err, result){
+        if (err) {
+          console.log("Upsert Error")
+          console.log(err);
+        } else {
+          console.log("Success >> " , result);
+          return  {
+            type: 'text',
+            text: 'เพิ่ม ' + displayname + ' ใน PYT ' + txt +'K แล้ว'
+        }
+        }
+      })
   } else {
-    Pyt.update({line_userId: uid}, {$set: {line_userId: uid, line_displayName: displayname, distance: txt}}, {upsert: true}, function(err, result){
-      if (err) {
-        console.log("Upsert Error")
-        console.log(err);
-      } else {
-        console.log("Success >> " , result);
-        return  {
-          type: 'text',
-          text: 'เพิ่ม ' + displayname + ' ใน PYT ' + txt +'K แล้ว'
-      }
-      }
-    })
+    return  {
+      type: 'text',
+      text: 'กรุณาใส่เฉพาะตัวเลข\n22 44 66 100'
+    }
   }
-
   }
-
 };
+
 
 
 module.exports = Race;
