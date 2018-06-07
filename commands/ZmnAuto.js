ea1afc66a62576205d0ccdf44939766299237945
Witchawat Burapavong <witchawat@Tanyathons-MacBook-Pro.local>
2018-06-07 14:05:10 +0700

    to ee

--- commands/ZmnAuto.js
+++ commands/ZmnAuto.js
@@ -131,8 +131,8 @@ const Cmd = function() {
     );
 
     _this.emit('pushMessage', {
-      // to: 'C9484e01ebf9cc46a2f17a523354704f9', //EE Classified
-      to: 'Uf1763382b8cc53af0669ca2d44f880a0', // to Ong
+      to: 'C9484e01ebf9cc46a2f17a523354704f9', //EE Classified
+      // to: 'Uf1763382b8cc53af0669ca2d44f880a0', // to Ong
       message: {
         type: 'text',
         text: zmnMsg
