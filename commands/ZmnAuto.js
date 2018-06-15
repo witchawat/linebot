be7577bd9465ea4cada5936c445c6d229b769c7e
witchawat <chiez39@gmail.com>
2018-06-15 14:57:50 +0700

    Update ZmnAuto.js

--- commands/ZmnAuto.js
+++ commands/ZmnAuto.js
@@ -169,7 +169,7 @@ const Cmd = function() {
     );
 
     _this.emit('pushMessage', {
-      to: 'C9484e01ebf9cc46a2f17a523354704f9', //EE Classified
+      to: 'C32654f9bd7a12ec3172dad4ff5bb5067', //EE Classified
       // to: 'Uf1763382b8cc53af0669ca2d44f880a0', // to Ong
       message: {
         type: 'text',
