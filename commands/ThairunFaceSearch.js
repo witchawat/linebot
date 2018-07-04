072377c7d5ab8c3e9a0bc6f737cb11cf7bbe7a13
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2018-07-04 13:35:27 +0700

    .

--- commands/ThairunFaceSearch.js
+++ commands/ThairunFaceSearch.js
@@ -6,7 +6,7 @@ const Cmd = function () {
   events.EventEmitter.call(this);
   const _this = this;
   this.handleEvent = function (evt, cmd, param) {
-    if (cmd == 'imgbase64') {
+    if (cmd == 'pk') {
       console.log('mangaImg');
       console.log(evt);
       axios.get(`https://api.line.me/v2/bot/message/${evt.message.id}/content`, {
@@ -16,11 +16,129 @@ const Cmd = function () {
         }
       }).then(r => {
         var b64 = Buffer.from(r.data, 'binary').toString('base64');
-        console.log(b64);
-        console.log(r.headers);
+        var postData = {
+          "operationName": "searchPhotosByFace",
+          "variables": {
+            "eventId": "PKRUNSS2",
+            "refData": {
+              "mimeType": "image/jpeg",
+              "base64data": b64
+            }
+          },
+          "query": `query searchPhotosByFace($eventId: MongoID, $refData: FileData!) {
+  searchPhotosByFace(eventId: $eventId, refData:$refData) {
+    count
+    items {
+      _id
+      similarity
+      event
+      photographer
+      fileName
+     downloadMode
+      info {
+        dateTimeTaken
+        __typename
+      }
+      view {
+        ...photoView
+       __typename
+      }
+      image {
+        small {
+          price
+          size {
+            width
+           height
+            __typename
+          }
+          __typename
+        }
+        medium {
+          price
+          size{
+            width
+            height
+            __typename
+          }
+          __typename
+        }
+        large{
+          price
+          size {
+            width
+            height
+            __typename
+          }
+         __typename
+        }
+        xlarge {
+          price
+          size {
+            width
+            height
+           __typename
+          }
+          __typename
+        }
+        A4 {
+          price
+          size {
+           width
+            height
+            __typename
+          }
+          __typename
+        }
+        __typename
+      }
+     photographer_ {
+        _id
+        displayName
+        username
+        __typename
+      }
+      __typename
+    }
+   __typename
+  }
+}
+
+fragment photoView on PhotoView {
+  preview {
+    url
+    size {
+      width
+      height
+     __typename
+    }
+    __typename
+  }
+  thumbnail {
+    url
+    size {
+      width
+      height
+      __typename
+    }
+   __typename
+  }
+  __typename
+}
+`
+        }
+        axios.post('https://api.photo.thai.run/graphql', postData, {
+          'headers': {
+            'Origin': 'https://photo.thai.run'
+          }
+        }).then(r => {
+          console.log(r.data);
+        }).catch(e => {
+          console.error('error calling thairun');
+          //console.log(e);
+        });
       }).catch(e => {
         console.error('get img from line error');
-        console.log(e);
+        //console.log(e);
       });
     }
   }
