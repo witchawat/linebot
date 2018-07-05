5c53b907e3e2c4fa0950e4d32d6f0a438b60d179
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2018-07-05 11:20:42 +0700

    .

--- commands/ThairunFaceSearch.js
+++ commands/ThairunFaceSearch.js
@@ -6,79 +6,92 @@ const Cmd = function () {
   events.EventEmitter.call(this);
   const _this = this;
   this.handleEvent = function (evt, cmd, param) {
+    var replyId = evt.source.userId;
+    replyId = (evt.source.roomId) ? evt.source.roomId : replyId;
+    replyId = (evt.source.groupId) ? evt.source.groupId : replyId;
+    var picCount = 3;
+    if (!isNaN(param * 1)) picCount = param * 1;
+    picCount = (picCount < 1) ? 1 : picCount;
+    picCount = (picCount > 20) ? 20 : picCount;
     if (cmd == 'pk') {
-      console.log('param :: '+param);
-      axios.get(`https://api.line.me/v2/bot/message/${evt.message.id}/content`, {
-        responseType: 'arraybuffer',
-        headers: {
-          Authorization: 'Bearer ' + process.env.LINEACCESS
-        }
-      }).then(r => {
-        var postData = {
-          "operationName": "searchPhotosByFace",
-          "variables": {
-            "eventId": "PKRUNSS2",
-            "refData": {
-              "mimeType": "image/jpeg",
-              "base64data": Buffer.from(r.data, 'binary').toString('base64')
-            }
-          },
-          "query": `query searchPhotosByFace($eventId: MongoID, $refData: FileData!) {
-  searchPhotosByFace(eventId: $eventId, refData:$refData) {
-    items {
-      similarity
-      view {
-        ...photoView
-      }
+      console.log(evt);
+      console.log('param :: ' + param);
+      searchFace('PKRUNSS2', replyId, picCount, Buffer.from(r.data, 'binary').toString('base64'));
     }
   }
+
+  function searchFace(raceName, replyId, picCount, base64img) {
+    axios.get(`https://api.line.me/v2/bot/message/${evt.message.id}/content`, {
+      responseType: 'arraybuffer',
+      headers: {
+        Authorization: 'Bearer ' + process.env.LINEACCESS
+      }
+    }).then(r => {
+      var postData = {
+        "operationName": "searchPhotosByFace",
+        "variables": {
+          "eventId": raceName,
+          "refData": {
+            "mimeType": "image/jpeg",
+            "base64data": base64img
+          }
+        },
+        "query": `
+query searchPhotosByFace($eventId: MongoID, $refData: FileData!) {
+searchPhotosByFace(eventId: $eventId, refData:$refData) {
+  items {
+    similarity
+    view {
+      ...photoView
+    }
+  }
+}
 }
 
 fragment photoView on PhotoView {
-  preview {
-    url
-  }
+preview {
+  url
+}
 }
 `
-        }
-        axios.post('https://api.photo.thai.run/graphql', postData).then(r => {
-          var imgs = [],
-            msg = 'ไม่พบภาพ';
-          console.log(r.data);
-          if (r.data.data.searchPhotosByFace) r.data.data.searchPhotosByFace.items.forEach(i => {
-            imgs.push({
-              score: i.similarity,
-              url: i.view.preview.url
-            });
+      }
+      axios.post('https://api.photo.thai.run/graphql', postData).then(r => {
+        var imgs = [],
+          msg = 'ไม่พบภาพ';
+        console.log(r.data);
+        if (r.data.data.searchPhotosByFace) r.data.data.searchPhotosByFace.items.forEach(i => {
+          imgs.push({
+            score: i.similarity,
+            url: i.view.preview.url
           });
-          imgs = imgs.sort(function (a, b) {
-            return (a.score > b.score) ? -1 : 1;
-          });
-          /*
-            .filter(i => {
-            return i.score > 93
-          });
-          */
-          imgs=imgs.slice(0,20);
-          if (imgs.length) msg = imgs.map(i => {
-            return i.url
-          }).join(`\n\n`);
-          _this.emit('replyMessage', {
-            replyToken: evt.replyToken,
-            message: {
-              type: 'text',
-              text: msg
-            }
-          });
-        }).catch(e => {
-          console.error('error calling thairun');
-          console.log(e);
+        });
+        imgs = imgs.sort(function (a, b) {
+          return (a.score > b.score) ? -1 : 1;
+        });
+        /*
+          .filter(i => {
+          return i.score > 93
+        });
+        */
+        imgs = imgs.slice(0, picCount);
+        if (imgs.length) msg = imgs.map(i => {
+          return i.url
+        }).join(`\n\n`);
+        _this.emit('pushMessage', {
+          to: replyId,
+          message: {
+            type: 'text',
+            text: msg
+          }
         });
       }).catch(e => {
-        console.error('get img from line error');
-        //console.log(e);
+        console.error('error calling thairun');
+        console.log(e);
       });
-    }
+    }).catch(e => {
+      console.error('get img from line error');
+      //console.log(e);
+    });
   }
   util.inherits(Cmd, events.EventEmitter);
 }
