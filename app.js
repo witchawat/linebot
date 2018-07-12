f4513bff45d9f7a092830d59ab39016974087b36
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2018-07-12 13:16:59 +0700

    marine marathon face search test

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-'use strict';
-require('dotenv').config();
-const LineEventHandler = require('./commands/EventHandler.js');
-const Rain = require('./commands/Rain.js');
-const Air = require('./commands/Air.js');
-const MyLog = require('./commands/MyLog.js');
-const WolframSolve = require('./commands/WolframSolve.js');
-const Weather = require('./commands/Weather.js');
-const Manga = require('./commands/Manga.js');
-const ThairunFaceSearch = require('./commands/ThairunFaceSearch.js');
+"use strict";
+require("dotenv").config();
+const LineEventHandler = require("./commands/EventHandler.js");
+const Rain = require("./commands/Rain.js");
+const Air = require("./commands/Air.js");
+const MyLog = require("./commands/MyLog.js");
+const WolframSolve = require("./commands/WolframSolve.js");
+const Weather = require("./commands/Weather.js");
+const Manga = require("./commands/Manga.js");
+const ThairunFaceSearch = require("./commands/ThairunFaceSearch.js");
 //const UTMF = require('./commands/Utmf.js');
-const Zmn = require('./commands/Zmn.js');
-const ZmnAuto= (process.env.NODE_ENV == 'production')?require('./commands/ZmnAuto.js'):null;
-const jsdom = require('jsdom');
-const {
-  JSDOM
-} = jsdom;
-const express = require('express');
-const bodyParser = require('body-parser');
-var CronJob = require('cron').CronJob;
-const line = require('@line/bot-sdk');
+const Zmn = require("./commands/Zmn.js");
+const ZmnAuto =
+  process.env.NODE_ENV == "production"
+    ? require("./commands/ZmnAuto.js")
+    : null;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const express = require("express");
+const bodyParser = require("body-parser");
+var CronJob = require("cron").CronJob;
+const line = require("@line/bot-sdk");
 //================================
 //        KEYS
 //================================
@@ -29,112 +30,126 @@ const config = {
 };
 const client = new line.Client(config);
 var eventHandler = new LineEventHandler(client);
-eventHandler.add(['rain', 'rainvid'], new Rain());
-var airHandler=new Air();
-eventHandler.add('air', airHandler);
-eventHandler.add('airloc', airHandler,'location');
-eventHandler.add('log', new MyLog());
-eventHandler.add('solve', new WolframSolve());
+eventHandler.add(["rain", "rainvid"], new Rain());
+var airHandler = new Air();
+eventHandler.add("air", airHandler);
+eventHandler.add("airloc", airHandler, "location");
+eventHandler.add("log", new MyLog());
+eventHandler.add("solve", new WolframSolve());
 eventHandler.add(
-  ['weather', 'w1', 'w2', 'w3', 'w4', 'w5', 'w6'], new Weather());
-  eventHandler.add('pk', new ThairunFaceSearch(),'image');
+  ["weather", "w1", "w2", "w3", "w4", "w5", "w6"],
+  new Weather()
+);
+eventHandler.add(["pk", "marine"], new ThairunFaceSearch(), "image");
 //eventHandler.add('utmf', new UTMF());
-eventHandler.add('zmn', new Zmn());
-if (process.env.NODE_ENV == 'production') eventHandler.add('zmnauto', new ZmnAuto());
+eventHandler.add("zmn", new Zmn());
+if (process.env.NODE_ENV == "production")
+  eventHandler.add("zmnauto", new ZmnAuto());
 const app = express();
-app.use(express.static('public'));
-app.use('/manga',bodyParser.urlencoded({
-  extended: true
-}));
-app.use('/manga',bodyParser.json());
-app.set('port', process.env.PORT || 5000);
-app.post('/webhook', line.middleware(config), (req, res) => {
+app.use(express.static("public"));
+app.use(
+  "/manga",
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use("/manga", bodyParser.json());
+app.set("port", process.env.PORT || 5000);
+app.post("/webhook", line.middleware(config), (req, res) => {
   req.body.events.forEach(evt => eventHandler.handleEvent(evt));
-  res.send('');
+  res.send("");
 });
-var mangaHandler=new Manga(app);
-eventHandler.add('manga',mangaHandler);
+var mangaHandler = new Manga(app);
+eventHandler.add("manga", mangaHandler);
 //eventHandler.logRules();
-app.get('/test/:q', (req, res) => {
-  if (process.env.NODE_ENV == 'development') {
+app.get("/test/:q", (req, res) => {
+  if (process.env.NODE_ENV == "development") {
     eventHandler.handleEvent({
-      type: 'message',
-      replyToken: '0',
+      type: "message",
+      replyToken: "0",
       source: {
-        userId: '1',
-        type: 'user'
+        userId: "1",
+        type: "user"
       },
       timestamp: 1524631288368,
       message: {
-        type: 'text',
-        id: '2',
+        type: "text",
+        id: "2",
         text: req.params.q
       }
     });
   }
-  res.send('Ong Line Bot');
+  res.send("Ong Line Bot");
 });
-app.get('/utmfRunner', (req, res) => {
+app.get("/utmfRunner", (req, res) => {
   // let url = 'https://utmb.livetrail.net/coureur.php?rech=' + req.query.bib;
-  let url = 'https://utmf.livetrail.net/coureur.php?rech=' + req.query.bib;
+  let url = "https://utmf.livetrail.net/coureur.php?rech=" + req.query.bib;
   console.log(url);
-  let web = JSDOM.fromURL(url).then(dom => {
-    console.log(dom.window.document);
-    let data = dom.window.document;
-    if (!data.querySelector('state')) {
-      res.status(404).send(';)');
-      console.log('not found');
-      return;
-    }
-    let state = data.querySelector('state').getAttribute('code');
-    if (state === 'a') {
-      state = 'DNF';
-    } else if (state === 'f') {
-      state = 'FINISHED';
-    } else {
-      state = null;
-    }
-    let last = -1;
-    let cp = [];
-    let pts = data.querySelectorAll('pts pt').forEach(row => {
-      cp.push({
-        idpt: row.getAttribute('idpt'),
-        n: row.getAttribute('n'),
-        nc: row.getAttribute('nc'),
-        km: row.getAttribute('km'),
-        nc: row.getAttribute('nc')
+  let web = JSDOM.fromURL(url)
+    .then(dom => {
+      console.log(dom.window.document);
+      let data = dom.window.document;
+      if (!data.querySelector("state")) {
+        res.status(404).send(";)");
+        console.log("not found");
+        return;
+      }
+      let state = data.querySelector("state").getAttribute("code");
+      if (state === "a") {
+        state = "DNF";
+      } else if (state === "f") {
+        state = "FINISHED";
+      } else {
+        state = null;
+      }
+      let last = -1;
+      let cp = [];
+      let pts = data.querySelectorAll("pts pt").forEach(row => {
+        cp.push({
+          idpt: row.getAttribute("idpt"),
+          n: row.getAttribute("n"),
+          nc: row.getAttribute("nc"),
+          km: row.getAttribute("km"),
+          nc: row.getAttribute("nc")
+        });
       });
-    });
-    let pass = data.querySelectorAll('pass e').forEach((row, i) => {
-      Object.assign(cp[i], {
-        racetime: row.getAttribute('tps'),
-        rank: row.getAttribute('clt'),
-        worldtime: row.getAttribute('hd') || row.getAttribute('ha')
+      let pass = data.querySelectorAll("pass e").forEach((row, i) => {
+        Object.assign(cp[i], {
+          racetime: row.getAttribute("tps"),
+          rank: row.getAttribute("clt"),
+          worldtime: row.getAttribute("hd") || row.getAttribute("ha")
+        });
+        last = i;
       });
-      last = i;
+      // Last CP REACHED
+      let runner = {
+        name:
+          data.querySelector("identite").getAttribute("prenom") +
+          " " +
+          data.querySelector("identite").getAttribute("nom"),
+        course: data
+          .querySelector("fiche")
+          .getAttribute("c")
+          .toUpperCase(),
+        country: data.querySelector("identite").getAttribute("nat"),
+        status: state,
+        last_update: cp[last] || undefined
+      };
+      res.status(200).json({
+        runner,
+        data: cp
+      });
+    })
+    .catch(error => {
+      console.log(error);
     });
-    // Last CP REACHED
-    let runner = {
-      name: data.querySelector('identite').getAttribute('prenom') + ' ' + data.querySelector('identite').getAttribute('nom'),
-      course: data.querySelector('fiche').getAttribute('c').toUpperCase(),
-      country: data.querySelector('identite').getAttribute('nat'),
-      status: state,
-      last_update: cp[last] || undefined
-    };
-    res.status(200).json({
-      runner,
-      data: cp
-    });
-  }).catch(error => {
-    console.log(error);
-  });
 });
-app.get('*', function (req, res) {
-  res.send('Ong Line Bot');
+app.get("*", function(req, res) {
+  res.send("Ong Line Bot");
 });
 //Heroku setting
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
 // new CronJob({
 //   cronTime: '0 0,10,20,30,40,50 * * * *',
--- commands/ThairunFaceSearch.js
+++ commands/ThairunFaceSearch.js
@@ -1,40 +1,45 @@
-const MAX_RETURN_PICS=40;
-var axios = require('axios');
-const util = require('util');
-const events = require('events');
-const Cmd = function () {
+const MAX_RETURN_PICS = 40;
+var axios = require("axios");
+const util = require("util");
+const events = require("events");
+const Cmd = function() {
   events.EventEmitter.call(this);
   const _this = this;
-  this.handleEvent = function (evt, cmd, param) {
+  this.handleEvent = function(evt, cmd, param) {
     var replyId = evt.source.userId;
-    replyId = (evt.source.roomId) ? evt.source.roomId : replyId;
-    replyId = (evt.source.groupId) ? evt.source.groupId : replyId;
+    replyId = evt.source.roomId ? evt.source.roomId : replyId;
+    replyId = evt.source.groupId ? evt.source.groupId : replyId;
     var picCount = 3;
     if (!isNaN(param * 1)) picCount = param * 1;
-    picCount = (picCount < 1) ? 1 : picCount;
-    picCount = (picCount > MAX_RETURN_PICS) ? MAX_RETURN_PICS : picCount;
-    if (cmd == 'pk') {
-      searchFace('PKRUNSS2', replyId, picCount, evt.message.id);
+    picCount = picCount < 1 ? 1 : picCount;
+    picCount = picCount > MAX_RETURN_PICS ? MAX_RETURN_PICS : picCount;
+    if (cmd == "pk") {
+      searchFace("PKRUNSS2", replyId, picCount, evt.message.id);
     }
-  }
+    if (cmd == "marine") {
+      searchFace("marines-marathon-2018", replyId, picCount, evt.message.id);
+    }
+  };
 
   function searchFace(raceName, replyId, picCount, contentId) {
-    axios.get(`https://api.line.me/v2/bot/message/${contentId}/content`, {
-      responseType: 'arraybuffer',
-      headers: {
-        Authorization: 'Bearer ' + process.env.LINEACCESS
-      }
-    }).then(r => {
-      var postData = {
-        "operationName": "searchPhotosByFace",
-        "variables": {
-          "eventId": raceName,
-          "refData": {
-            "mimeType": "image/jpeg",
-            "base64data": Buffer.from(r.data, 'binary').toString('base64')
-          }
-        },
-        "query": `
+    axios
+      .get(`https://api.line.me/v2/bot/message/${contentId}/content`, {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: "Bearer " + process.env.LINEACCESS
+        }
+      })
+      .then(r => {
+        var postData = {
+          operationName: "searchPhotosByFace",
+          variables: {
+            eventId: raceName,
+            refData: {
+              mimeType: "image/jpeg",
+              base64data: Buffer.from(r.data, "binary").toString("base64")
+            }
+          },
+          query: `
 query searchPhotosByFace($eventId: MongoID, $refData: FileData!) {
   searchPhotosByFace(eventId: $eventId, refData:$refData) {
     items {
@@ -55,62 +60,67 @@ fragment photoView on PhotoView {
   }
 }
 `
-      }
-      axios.post('https://api.photo.thai.run/graphql', postData).then(r => {
-        var imgs = [],
-          msg = 'ไม่พบภาพ';
-        if (r.data.data.searchPhotosByFace) r.data.data.searchPhotosByFace.items.forEach(i => {
-          imgs.push({
-            score: i.similarity,
-            url: i.view.preview.url,
-            tmb: i.view.thumbnail.url
-          });
-        });
-        imgs = imgs.sort(function (a, b) {
-          return (a.score > b.score) ? -1 : 1;
-        });
-        /*
+        };
+        axios
+          .post("https://api.photo.thai.run/graphql", postData)
+          .then(r => {
+            var imgs = [],
+              msg = "ไม่พบภาพ";
+            if (r.data.data.searchPhotosByFace)
+              r.data.data.searchPhotosByFace.items.forEach(i => {
+                imgs.push({
+                  score: i.similarity,
+                  url: i.view.preview.url,
+                  tmb: i.view.thumbnail.url
+                });
+              });
+            imgs = imgs.sort(function(a, b) {
+              return a.score > b.score ? -1 : 1;
+            });
+            /*
           .filter(i => {
           return i.score > 93
         });
         */
-        imgs = imgs.slice(0, picCount);
-        if (imgs.length) {
-          imgs.map(i => {
-            _this.emit('pushMessage', {
+            imgs = imgs.slice(0, picCount);
+            if (imgs.length) {
+              imgs.map(i => {
+                _this.emit("pushMessage", {
+                  to: replyId,
+                  message: {
+                    type: "image",
+                    originalContentUrl: i.url,
+                    previewImageUrl: i.tmb
+                  }
+                });
+              });
+              return;
+            }
+            _this.emit("pushMessage", {
               to: replyId,
               message: {
-                "type": "image",
-                "originalContentUrl": i.url,
-                "previewImageUrl": i.tmb
+                type: "text",
+                text: "ไม่พบภาพ"
               }
             });
+          })
+          .catch(e => {
+            console.error("error calling thairun");
+            _this.emit("pushMessage", {
+              to: replyId,
+              message: {
+                type: "text",
+                text: "ไม่พบภาพ"
+              }
+            });
+            //console.log(e);
           });
-          return;
-        }
-        _this.emit('pushMessage', {
-          to: replyId,
-          message: {
-            type: 'text',
-            text: 'ไม่พบภาพ'
-          }
-        });
-      }).catch(e => {
-        console.error('error calling thairun');
-        _this.emit('pushMessage', {
-          to: replyId,
-          message: {
-            type: 'text',
-            text: 'ไม่พบภาพ'
-          }
-        });
+      })
+      .catch(e => {
+        console.error("get img from line error");
         //console.log(e);
       });
-    }).catch(e => {
-      console.error('get img from line error');
-      //console.log(e);
-    });
   }
   util.inherits(Cmd, events.EventEmitter);
-}
+};
 module.exports = Cmd;
