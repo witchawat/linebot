f1d96d4ea7d9e46e9bf3618febe7408b8b59656c
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2018-11-10 12:08:42 +0700

    .

--- commands/Pyt.js
+++ commands/Pyt.js
@@ -249,7 +249,7 @@ const Cmd = function() {
       ret += `${pinEmoji} (${runner.lastCp}) [${runner.km} / ${runner.maxKM} km] ${runner.raceTime} : ${runner.timeOfDay}`;
     }
     if (runner.rank) {
-      ret += `  ${crownEmoji} overall: ${runner.rank.overall} ♂ gender: ${runner.rank.gender}`;
+      ret += `  ${crownEmoji}:   ${runner.rank.overall}  ♂♀:   ${runner.rank.gender}`;
     }
     return ret;
   }
