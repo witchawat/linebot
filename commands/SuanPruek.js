b093e00f9482e742b69531822ae50c4ddfa9bdac
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2019-04-26 20:23:19 +0700

    .

--- commands/SuanPruek.js
+++ commands/SuanPruek.js
@@ -1,23 +1,24 @@
-const util = require('util');
-const events = require('events');
+const util = require("util");
+const events = require("events");
 const Cmd = function() {
   events.EventEmitter.call(this);
   const _this = this;
   this.handleEvent = function(evt, cmd, param) {
-    console.log(evt);
-    _this.emit('replyMessage', {
-      replyToken: evt.replyToken,
-      message: {
-        type: 'text',
-        text:
-          'userId : ' +
-          evt.source.userId +
-          '\nroomId : ' +
-          evt.source.roomId +
-          '\ngroupId : ' +
-          evt.source.groupId
-      }
-    });
+    if (cmd == "settime") {
+      var raceClk = param.split(":").map(v => v * 1);
+      var t = 10 * 60 * 60 * 1000 - ((raceClk[0] * 60 + raceClk[1]) * 60 + raceClk[2]) * 1000;
+      var d = new Date();
+      console.log(d);
+      d.setTime(d.getTime() + t);
+      console.log(d);
+      _this.emit("replyMessage", {
+        replyToken: evt.replyToken,
+        message: {
+          type: "text",
+          text: d.toISOString()
+        }
+      });
+    }
   };
   util.inherits(Cmd, events.EventEmitter);
 };
