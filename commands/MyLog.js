26e93afbdc50a7441fda5005e7f0196eaf717aaa
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2019-06-10 19:36:42 +0700

    .

--- commands/MyLog.js
+++ commands/MyLog.js
@@ -1,3 +1,4 @@
+var axios = require("axios");
 const util = require("util");
 const events = require("events");
 const Cmd = function() {
@@ -5,21 +6,32 @@ const Cmd = function() {
   const _this = this;
   this.handleEvent = function(evt, cmd, param) {
     console.log(evt);
-    _this.emit("replyMessage", {
-      replyToken: evt.replyToken,
-      message: {
-        type: "text",
-        text:
-          "userId : " +
-          evt.source.userId +
-          "\nroomId : " +
-          evt.source.roomId +
-          "\ngroupId : " +
-          evt.source.groupId +
-          "\nparam : " +
-          param
-      }
-    });
+    axios
+      .get(`https://api.line.me/v2/bot/room/${evt.source.roomId}/members/ids`, {
+        headers: {
+          Authorization: "Bearer " + process.env.LINEACCESS
+        }
+      })
+      .then(r => {
+        _this.emit("replyMessage", {
+          replyToken: evt.replyToken,
+          message: {
+            type: "text",
+            text:
+              "userId : " +
+              evt.source.userId +
+              "\nroomId : " +
+              evt.source.roomId +
+              "\ngroupId : " +
+              evt.source.groupId +
+              "\nparam : " +
+              param +
+              "\nres : " +
+              JSON.stringify(r.data)
+          }
+        });
+      })
+      .catch(e => console.error(e));
   };
   util.inherits(Cmd, events.EventEmitter);
 };
