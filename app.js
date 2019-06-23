5e1ed71517295c821f04e444bb0bb9e9205b3496
pantoffski <chaichana.n@gmail.com>
2019-06-23 19:13:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ eventHandler.add(
   new WeatherDarkSky()
 );
 eventHandler.add(["10hr"], new ThairunFaceSearch(), "image");
-eventHandler.add("zmn", new Zmn());
+//eventHandler.add("zmn", new Zmn());
 eventHandler.add("ff", new FitnessFirst());
 // eventHandler.add(
 //   ["wweather", "ww", "ww1", "ww2", "ww3", "ww4", "ww5", "ww6", "ww7", "ww8", "ww9", "ww10"],
