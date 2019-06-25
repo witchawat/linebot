66fb973743cd0e77717dbeadafaa31e2eb1c91e2
Witchawat Burapavong <witchawat@Witchawats-MacBook-Pro.local>
2019-06-25 10:49:32 +0700

    params

--- commands/FitnessFirst.js
+++ commands/FitnessFirst.js
@@ -17,44 +17,22 @@ const Cmd = function() {
   */
   this.handleEvent = function(evt, cmd, param) {
     if (!param) {
-      axios
-        .get(
-          "https://www.fitnessfirst.co.th/fitness-first/web-services/v2/timetable/%7B03D82E6E-083F-4F37-B877-9CAFC75D919C%7D/" +
-            searchBranch
-        )
-        .then(r => {
-          const cls = r.data.Timetable;
-          // Filter only Today's CLASS
-          let mtoday = cls.Morning.filter(c => c.IsToday == true);
-          let atoday = cls.Afternoon.filter(c => c.IsToday == true);
-          let etoday = cls.Evening.filter(c => c.IsToday == true);
-
-          let m = findClassByName(mtoday[0].Classes, searchClass);
-          let a = findClassByName(atoday[0].Classes, searchClass);
-          let e = findClassByName(etoday[0].Classes, searchClass);
-
-          let result = m.concat(a, e);
-          let resultText = "";
-          // Result to Text
-          result.forEach(c => {
-            let time = c.TimeText.slice(0, 8);
-            let club = c.ClubTag;
-            let title = c.Title;
-            resultText += title + " " + time + " " + club + "\n";
-          });
-          _this.emit("replyMessage", {
-            replyToken: evt.replyToken,
-            message: {
-              type: "text",
-              text: resultText
-            }
-          });
-        });
+      getClass(searchBranch, searchClass);
     } else {
       // with params here
+      if (param != "all") {
+        let branches = param.toUpperCase().split(/[\s,]+/);
+        searchBranch = branches.toString();
+        getClass(searchBranch, searchClass);
+      }
+      if (param == "all") {
+        searchBranch =
+          "P39,ICS,CTW,AIA,LMP,QHL,SSQ,SPG,CBN,CHW,CBR,RM9,CKK,CPK,RM2,RM3,RTB,CUD,FPK,PKS,MBN,SCS,T21,CRD,CRP,BKP,BKE,KRT,NWW,MTP,PRM";
+        getClass(searchBranch, searchClass);
+      }
     }
   };
-  util.inherits(Cmd, events.EventEmitter);
+  util.inherits(Cmdevents.EventEmitter);
 };
 module.exports = Cmd;
 
@@ -64,4 +42,40 @@ function findClassByName(classes, name) {
     c.ClassTypeTag == name ? result.push(c) : result;
   });
   return result;
+}
+
+function getClass(branch, workout) {
+  axios
+    .get(
+      "https://www.fitnessfirst.co.th/fitness-first/web-services/v2/timetable/%7B03D82E6E-083F-4F37-B877-9CAFC75D919C%7D/" +
+        branch
+    )
+    .then(r => {
+      const cls = r.data.Timetable;
+      // Filter only Today's CLASS
+      let mtoday = cls.Morning.filter(c => c.IsToday == true);
+      let atoday = cls.Afternoon.filter(c => c.IsToday == true);
+      let etoday = cls.Evening.filter(c => c.IsToday == true);
+
+      let m = findClassByName(mtoday[0].Classes, workout);
+      let a = findClassByName(atoday[0].Classes, workout);
+      let e = findClassByName(etoday[0].Classes, workout);
+
+      let result = m.concat(a, e);
+      let resultText = "";
+      // Result to Text
+      result.forEach(c => {
+        let time = c.TimeText.slice(0, 8);
+        let club = c.ClubTag;
+        let title = c.Title;
+        resultText += title + " " + time + " " + club + "\n";
+      });
+      _this.emit("replyMessage", {
+        replyToken: evt.replyToken,
+        message: {
+          type: "text",
+          text: "FF @ " + branch + "\n" + resultText
+        }
+      });
+    });
 }
