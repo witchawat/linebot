d175fb699238a69106b9856eb2069ef24d2a2624
Witchawat Burapavong <witchawat@Witchawats-MacBook-Pro.local>
2019-06-28 13:18:36 +0700

    change radar source

--- commands/Rain.js
+++ commands/Rain.js
@@ -1,153 +1,173 @@
-var request = require('request');
-var CronJob = require('cron').CronJob;
-const util = require('util');
-const events = require('events');
-const Cmd = function () {
+var request = require("request");
+var CronJob = require("cron").CronJob;
+const util = require("util");
+const events = require("events");
+const Cmd = function() {
   events.EventEmitter.call(this);
-  var lastUpdate = new Date('2017-01-01'),
-    vidStat = 'error',
-    imgStat = 'error',
-    imgTmb = '',
-    imgUrl = '',
-    vidTmb = '',
-    vidUrl = '';
+  var lastUpdate = new Date("2017-01-01"),
+    vidStat = "error",
+    imgStat = "error",
+    imgTmb = "",
+    imgUrl = "",
+    vidTmb = "",
+    vidUrl = "";
   const _this = this;
-  this.handleEvent = function (evt, cmd, param) {
+  this.handleEvent = function(evt, cmd, param) {
     var ret = {};
-    if (cmd == 'rain') {
-      if (imgStat == 'error') ret = {
-        "type": 'text',
-        'text': 'ไม่สามารถ load รูปได้ รบกวนไปดูเองที่\r\nhttp://weather.bangkok.go.th/FTPCustomer/radar/pics/zfiltered.jpg'
-      }
-      else ret = {
-        "type": 'image',
-        "originalContentUrl": imgUrl,
-        "previewImageUrl": imgTmb
-      }
+    if (cmd == "rain") {
+      if (imgStat == "error")
+        ret = {
+          type: "text",
+          text:
+            "ไม่สามารถ load รูปได้ รบกวนไปดูเองที่\r\nhttp://weather.bangkok.go.th/Images/Radar/nkradar.jpg"
+        };
+      else
+        ret = {
+          type: "image",
+          originalContentUrl: imgUrl,
+          previewImageUrl: imgTmb
+        };
     }
-    if (cmd == 'rainvid') {
-      if (vidStat == 'error') ret = {
-        "type": 'text',
-        "text": "ไม่สามารถ load gif ได้ รบกวนไปดูเองที่\r\nhttp://203.155.220.231/Radar/pics/nkradar.gif"
-      }
-      else ret = {
-        "type": "video",
-        "originalContentUrl": vidUrl,
-        "previewImageUrl": (imgStat == 'error') ? vidTmb : imgTmb
-      }
+    if (cmd == "rainvid") {
+      if (vidStat == "error")
+        ret = {
+          type: "text",
+          text:
+            "ไม่สามารถ load gif ได้ รบกวนไปดูเองที่\r\nhttp://203.155.220.231/Radar/pics/nkradar.gif"
+        };
+      else
+        ret = {
+          type: "video",
+          originalContentUrl: vidUrl,
+          previewImageUrl: imgStat == "error" ? vidTmb : imgTmb
+        };
     }
-    _this.emit('replyMessage', {
+    _this.emit("replyMessage", {
       replyToken: evt.replyToken,
       message: ret
     });
-  }
+  };
 
   function gfyAuth() {
     return new Promise((resolve, reject) => {
       var data = {
-        "client_id": process.env.GFY_ID,
-        "client_secret": process.env.GFY_SECRET,
-        "grant_type": "client_credentials"
-      }
-      request({
-        headers: {
-          'Content-Type': 'application/json'
+        client_id: process.env.GFY_ID,
+        client_secret: process.env.GFY_SECRET,
+        grant_type: "client_credentials"
+      };
+      request(
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          url: "https://api.gfycat.com/v1/oauth/token",
+          method: "POST",
+          body: data,
+          json: true
         },
-        url: 'https://api.gfycat.com/v1/oauth/token',
-        method: 'POST',
-        body: data,
-        json: true
-      }, function (err, _res, body) {
-        if (err) reject('');
-        if (body) resolve(body.access_token);
-        reject('');
-      });
+        function(err, _res, body) {
+          if (err) reject("");
+          if (body) resolve(body.access_token);
+          reject("");
+        }
+      );
     });
   }
 
   function getGfy(gfyname, token) {
     return new Promise((resolve, reject) => {
       var _this = this;
-      request({
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token
+      request(
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+          },
+          url: "https://api.gfycat.com/v1/gfycats/" + gfyname,
+          method: "GET",
+          json: true
         },
-        url: 'https://api.gfycat.com/v1/gfycats/' + gfyname,
-        method: 'GET',
-        json: true
-      }, function (err, _res, body) {
-        if (err) reject('');
-        if (body.gfyItem) {
-          //console.log(body.gfyItem);
-          resolve(body.gfyItem);
-        } else {
-          reject('');
+        function(err, _res, body) {
+          if (err) reject("");
+          if (body.gfyItem) {
+            //console.log(body.gfyItem);
+            resolve(body.gfyItem);
+          } else {
+            reject("");
+          }
         }
-      });
+      );
     });
   }
 
   function getGfyStat(gfyname, token) {
     return new Promise((resolve, reject) => {
-      request({
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token
+      request(
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+          },
+          url: "https://api.gfycat.com/v1/gfycats/fetch/status/" + gfyname,
+          method: "GET",
+          json: true
         },
-        url: 'https://api.gfycat.com/v1/gfycats/fetch/status/' + gfyname,
-        method: 'GET',
-        json: true
-      }, function (err, _res, body) {
-        if (err) {
-          reject('');
-        }
-        if (body) {
-          if (body.gfyName) {
-            resolve(body.gfyName);
+        function(err, _res, body) {
+          if (err) {
+            reject("");
           }
-          if (body.gfyname) {
-            resolve(body.gfyname);
+          if (body) {
+            if (body.gfyName) {
+              resolve(body.gfyName);
+            }
+            if (body.gfyname) {
+              resolve(body.gfyname);
+            }
+            reject(body.time);
           }
-          reject(body.time);
         }
-      });
+      );
     });
   }
 
   function gfyPost(url) {
     return new Promise((resolve, reject) => {
       var data = {
-        fetchUrl: url + "?v=" + (new Date().getTime()),
-        title: 'Bangkok Weather ' + (new Date().getTime())
-      }
-      request({
-        headers: {
-          'Content-Type': 'application/json'
+        fetchUrl: url + "?v=" + new Date().getTime(),
+        title: "Bangkok Weather " + new Date().getTime()
+      };
+      request(
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          url: "https://api.gfycat.com/v1/gfycats",
+          method: "POST",
+          body: data,
+          json: true
         },
-        url: 'https://api.gfycat.com/v1/gfycats',
-        method: 'POST',
-        body: data,
-        json: true
-      }, function (err, _res, body) {
-        if (err) {
-          reject('');
+        function(err, _res, body) {
+          if (err) {
+            reject("");
+          }
+          if (body) {
+            resolve(body.gfyname);
+          }
+          reject("");
         }
-        if (body) {
-          resolve(body.gfyname);
-        }
-        reject('');
-      });
+      );
     });
   }
   async function genWeatherImgAndVid() {
-    console.log('genWeatherImgAndVid');
+    console.log("genWeatherImgAndVid");
     var gfyname, token, gfyObj;
     var checkCount;
     try {
       token = await gfyAuth();
       //post img
-      gfyname = await gfyPost('http://weather.bangkok.go.th/FTPCustomer/radar/pics/zfiltered.jpg');
+      gfyname = await gfyPost(
+        "http://weather.bangkok.go.th/FTPCustomer/radar/pics/zfiltered.jpg"
+      );
       checkCount = 0;
       while (checkCount < 10) {
         checkCount++;
@@ -159,17 +179,17 @@ const Cmd = function () {
           //console.log(gfyObj);
           imgTmb = gfyObj.mobilePosterUrl;
           imgUrl = gfyObj.posterUrl;
-          imgStat = 'ok';
+          imgStat = "ok";
           break;
         } catch (e) {
           await new Promise(resolve => setTimeout(resolve, 20000));
         }
       }
       if (checkCount == 10) {
-        imgStat = 'error';
+        imgStat = "error";
       }
       //post vid
-      gfyname = await gfyPost('http://203.155.220.231/Radar/pics/nkradar.gif');
+      gfyname = await gfyPost("http://203.155.220.231/Radar/pics/nkradar.gif");
       checkCount = 0;
       //check 10 times
       while (checkCount < 10) {
@@ -182,14 +202,14 @@ const Cmd = function () {
           //console.log(gfyObj);
           vidUrl = gfyObj.mobileUrl;
           vidTmb = gfyObj.mobilePosterUrl;
-          vidStat = 'ok';
+          vidStat = "ok";
           break;
         } catch (e) {
           await new Promise(resolve => setTimeout(resolve, 20000));
         }
       }
       if (checkCount == 10) {
-        vidStat = 'error';
+        vidStat = "error";
       }
       /*
       console.log(imgTmb);
@@ -197,20 +217,20 @@ const Cmd = function () {
       console.log(vidUrl);
       */
     } catch (e) {
-      console.log('error genWeatherImgAndVid !!');
+      console.log("error genWeatherImgAndVid !!");
       console.log(e);
-      return '';
+      return "";
     }
-    return 'ok';
+    return "ok";
   }
   util.inherits(Cmd, events.EventEmitter);
   new CronJob({
-    cronTime: '56 1,11,21,31,41,51 * * * *',
+    cronTime: "56 1,11,21,31,41,51 * * * *",
     onTick: genWeatherImgAndVid,
     start: true,
-    timeZone: 'Asia/Bangkok',
+    timeZone: "Asia/Bangkok",
     runOnInit: true
   });
   //genWeatherImgAndVid();
-}
+};
 module.exports = Cmd;
