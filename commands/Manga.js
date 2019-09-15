9a826471b3b36d1ea482f91b665b22a8c7835ce8
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2019-09-15 17:55:13 +0700

    .

--- commands/Manga.js
+++ commands/Manga.js
@@ -342,27 +342,6 @@ const Cmd = function(app) {
             backgroundColor: "#03303Acc",
             paddingAll: "20px",
             paddingTop: "18px"
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "SALE",
-                color: "#ffffff",
-                align: "center",
-                size: "xs",
-                offsetTop: "3px"
-              }
-            ],
-            position: "absolute",
-            cornerRadius: "20px",
-            offsetTop: "18px",
-            backgroundColor: "#ff334b",
-            offsetStart: "18px",
-            height: "25px",
-            width: "53px"
           }
         ],
         paddingAll: "0px"
