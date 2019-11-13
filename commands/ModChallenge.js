7e45e2326eea3d40f3a7b1f7e57fdb0af51a808b
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2019-11-13 14:12:22 +0700

    .

--- commands/ModChallenge.js
+++ commands/ModChallenge.js
@@ -5,17 +5,17 @@ const Cmd = function() {
   events.EventEmitter.call(this);
   const _this = this;
   this.handleEvent = function(evt, cmd, param) {
-    console.log('-----------mod challenge');
-    
-    var ids=evt.message.text.split(/[\s,]+/).splice(1);
-    
-    console.log(evt);
+    console.log("-----------mod challenge");
+
+    var ids = evt.message.text.split(/[\s,]+/).splice(1);
+
+    console.log(cmd, evt);
+    console.log(ids);
+
     if (cmd == "modFactorTo1") {
-      console.log({newMul:1,ids});
-      
+      console.log({ newMul: 1, ids });
     }
   };
   util.inherits(Cmd, events.EventEmitter);
 };
 module.exports = Cmd;
-
